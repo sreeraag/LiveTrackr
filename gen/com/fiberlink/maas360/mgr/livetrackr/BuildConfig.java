d8272a79d70f8e6c74386d356a115bced240ffec
sreeraag <sreeraag.sss@gmail.com>
2014-05-23 13:24:45 +0530

    Initial commit

---
+++ gen/com/fiberlink/maas360/mgr/livetrackr/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.fiberlink.maas360.mgr.livetrackr;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
